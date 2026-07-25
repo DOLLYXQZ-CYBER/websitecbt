d6d24398560b4d7ffdb35a64c9ff0dd35b62e2da
Tobiloba <tobilobasalawu04@gmail.com>
2026-07-25 06:40:53 -1100

    Create biology.js

---
+++ cbt/questions/biology.js
@@ -0,0 +1,58 @@
+const biologyQuestions = [
+
+{
+question: "What is the basic unit of life?",
+options: [
+"Tissue",
+"Cell",
+"Organ",
+"Organism"
+],
+answer: 1
+},
+
+{
+question: "Which organ pumps blood round the body?",
+options: [
+"Liver",
+"Lungs",
+"Heart",
+"Kidney"
+],
+answer: 2
+},
+
+{
+question: "Green plants make their food through?",
+options: [
+"Respiration",
+"Photosynthesis",
+"Digestion",
+"Transpiration"
+],
+answer: 1
+},
+
+{
+question: "Which blood vessel carries blood away from the heart?",
+options: [
+"Vein",
+"Artery",
+"Capillary",
+"Valve"
+],
+answer: 1
+},
+
+{
+question: "Which of these is a mammal?",
+options: [
+"Shark",
+"Snake",
+"Whale",
+"Frog"
+],
+answer: 2
+}
+
+];
