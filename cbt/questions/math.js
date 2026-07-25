764dcda6441c355a3ac2576f9b1db50a5b4bf4e6
Tobiloba <tobilobasalawu04@gmail.com>
2026-07-25 00:42:56 -1100

    Add math questions to math.js file

---
+++ cbt/questions/math.js
@@ -0,0 +1,58 @@
+const mathQuestions = [
+
+{
+question: "What is 12 × 8?",
+options: [
+"96",
+"88",
+"108",
+"92"
+],
+answer: 0
+},
+
+{
+question: "Solve: 25 + 18",
+options: [
+"42",
+"43",
+"44",
+"45"
+],
+answer: 1
+},
+
+{
+question: "Find the value of x: 5x = 40",
+options: [
+"5",
+"6",
+"7",
+"8"
+],
+answer: 3
+},
+
+{
+question: "What is √144?",
+options: [
+"11",
+"12",
+"13",
+"14"
+],
+answer: 1
+},
+
+{
+question: "Simplify: 3² + 4²",
+options: [
+"25",
+"49",
+"12",
+"16"
+],
+answer: 0
+}
+
+];
