f943b21da753c26a4ee6c1cf0adbaf4f035041de
Tobiloba <tobilobasalawu04@gmail.com>
2026-07-25 00:40:37 -1100

    Add English questions for quiz

---
+++ cbt/questions/english.js
@@ -0,0 +1,32 @@
+const englishQuestions = [
+{
+    question: "Choose the correct sentence.",
+    options: [
+        "He don't like football.",
+        "He doesn't like football.",
+        "He not like football.",
+        "He didn't likes football."
+    ],
+    answer: 1
+},
+{
+    question: "What is the synonym of 'Happy'?",
+    options: [
+        "Sad",
+        "Angry",
+        "Joyful",
+        "Weak"
+    ],
+    answer: 2
+},
+{
+    question: "Choose the correctly spelt word.",
+    options: [
+        "Succesful",
+        "Successful",
+        "Succesfull",
+        "Succesfulll"
+    ],
+    answer: 1
+}
+];
