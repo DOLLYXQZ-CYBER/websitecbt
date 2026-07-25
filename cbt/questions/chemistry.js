12019e219d13c4d3ec0462260d1c6d5534af78e2
Tobiloba <tobilobasalawu04@gmail.com>
2026-07-25 06:39:25 -1100

    Create chemistry.js

---
+++ cbt/questions/chemistry.js
@@ -0,0 +1,58 @@
+const chemistryQuestions = [
+
+{
+question: "Water is made up of which elements?",
+options: [
+"Hydrogen and Oxygen",
+"Hydrogen and Nitrogen",
+"Oxygen and Carbon",
+"Carbon and Hydrogen"
+],
+answer: 0
+},
+
+{
+question: "The chemical symbol for Sodium is?",
+options: [
+"So",
+"S",
+"Na",
+"N"
+],
+answer: 2
+},
+
+{
+question: "Which gas is released during photosynthesis?",
+options: [
+"Carbon dioxide",
+"Oxygen",
+"Nitrogen",
+"Hydrogen"
+],
+answer: 1
+},
+
+{
+question: "The pH of a neutral solution is?",
+options: [
+"0",
+"5",
+"7",
+"14"
+],
+answer: 2
+},
+
+{
+question: "Which of these is an acid?",
+options: [
+"Sodium Hydroxide",
+"Hydrochloric Acid",
+"Ammonia",
+"Calcium Oxide"
+],
+answer: 1
+}
+
+];
