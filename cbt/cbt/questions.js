b860755aed2457ba921877a76cc2033f56246b44
Tobiloba <tobilobasalawu04@gmail.com>
2026-07-26 01:15:03 -1100

    Add questionBank object to questions.js

---
+++ cbt/cbt/questions.js
@@ -0,0 +1,7 @@
+const questionBank = {
+    english: englishQuestions,
+    math: mathQuestions,
+    physics: physicsQuestions,
+    chemistry: chemistryQuestions,
+    biology: biologyQuestions
+};
