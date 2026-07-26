3b3704f0406af82c0c87e740aacc5452f4e4311f
Tobiloba <tobilobasalawu04@gmail.com>
2026-07-26 01:12:04 -1100

    Delete cbt/cbt directory

--- cbt/cbt/questions.js
+++
@@ -1,7 +0,0 @@
-const questionBank = {
-    english: englishQuestions,
-    math: mathQuestions,
-    physics: physicsQuestions,
-    chemistry: chemistryQuestions,
-    biology: biologyQuestions
-};
